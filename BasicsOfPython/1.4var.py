327d9d8c86a2ffddb6171e834a7dc5e95c77dede
Vipray Jain <viprayjain18@gmail.com>
2020-06-27 16:59:29 +0530

    Basics1

---
+++ BasicsOfPython/1.4var.py
@@ -0,0 +1,23 @@
+#var
+a=53444532654364444444444444444444444497834
+print(a)
+#type of a
+print(type(a))
+
+b=5.3444532654364444444444444444444444497834
+#print till certail limit after decimal
+print(b)
+#type of b
+print(type(b))
+
+b="Ramadheer"
+print(b)
+#type of b
+print(type(b))
+
+#-----------------------------#
+# Typecasting
+a=3.6
+print(int(a))
+
+
---
+++ BasicsOfPython/1.5operator.py
@@ -0,0 +1,13 @@
+print(2+4.5)
+print(3*8)
+
+#pow
+print(3**8)
+
+#float div
+print(3/7)
+
+#based on value type div
+print(31.00//7.0)
+print(31//7)
+print(8%5)
